e50204cb383129a31b42500c2ee00bbf38fe3289
MS269 <59796004+MS269@users.noreply.github.com>
2020-10-21 16:01:13 +0900

    10/21/20

---
+++ #677 Div. 3/A.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const vector<int> kFactorial{1, 1, 3, 6, 10};
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    string x;
+    cin >> x;
+    cout << 10 * (x[0] - '1') + kFactorial[x.size()] << '\n';
+  }
+  return 0;
+}
---
+++ #677 Div. 3/C.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int Find(const vector<int>& a) {
+  int maxa = *max_element(a.begin(), a.end()), n = a.size();
+  for (int i = 0; i < n; ++i) {
+    if (a[i] == maxa) {
+      if (i - 1 >= 0 && a[i - 1] < a[i]) return i + 1;
+      if (i + 1 < n && a[i + 1] < a[i]) return i + 1;
+    }
+  }
+  return -1;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& i : a) cin >> i;
+    cout << Find(a) << '\n';
+  }
+  return 0;
+}
